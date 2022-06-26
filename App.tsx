dc18d7b6d330a525bb8d4ebf0435a1e56f09efd6
Junhao Zhang <junhaodev@gmail.com>
2022-06-25 21:33:17 -0500

    having fun with buttons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,48 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, SafeAreaView, Image, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [dismiss, setDismiss] = useState(0);
+  const [shouldDo, setShouldDo] = useState(true);
+
+  const update = () => {
+    setCount(count + 1);
+    if (shouldDo) {
+      Alert.alert(
+        "You clicked on the button!",
+        "Thank you very much for clicking on the button, it means a lot to us. The energy you have spent will be wasted!",
+        [
+          {
+            text: "dismiss",
+            onPress: () => setDismiss(dismiss + 1)
+          },
+          {
+            text: "close without ratio"
+          },
+          {
+            text: "I didn't ask",
+            style: "cancel",
+            onPress: () => Alert.alert("rood")
+          },
+          {
+            text: "don't tell me again",
+            style: "destructive",
+            onPress: () => setShouldDo(false)
+          }
+        ]
+      );
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>{count} : {dismiss} ratio</Text>
+      <Image source={require("./assets/adaptive-icon.png")} style={styles.img} />
       <StatusBar hidden={true} />
-    </View>
+      <Text onPress={update} style={styles.btn}>Click me pseudo button</Text>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +50,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  img: {
+    width: 50,
+    height: 50
+  },
+  btn: {
+    backgroundColor: 'orange',
+    padding: 10,
+    display: 'flex',
+    width: 80,
+    borderRadius: 4,
+  }
 });
